acb120344e034dc31491f2ab2001396971df18d9
jackdixonryan <jack.dixon.ryan@gmail.com>
2022-08-11 16:10:45 -0600

    Some scaffolding for the kinds of commands that the system will need to take and process.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
 fn main() {
-    println!("Hello, world!");
+    println!("INGN SYSTEMS - MAIN - VERSION 0.0.1");
+    println!("ENTER AUTHORIZATION KEY...");
+    // here shall lie the authentication / authorization system 
+    // along with an integration with park staff data 
+    // and permissions APIs 
+
+    println!("WELCOME USER. BOOTING UP...");
+    // here shall lie the preparedness functions such as 
+    // - electrical systems can be turned on 
+    // = water systems can be turned on
+    // - ventilation systems can be turned on
+    // - security systems can be turned on 
+    // - door locks can be turned on 
+    // - databases are available for access
+    // - phone systems can be turned on 
+
+    // at this point, open shell should be running
+    // which can take commands. 
+
+    // Some commands that we know of:
+        // access - to get access to a particular system in the list mentioned above
+        // exit - to exit the shell
+        // help - to navigate the shell
+        // shutdown - to shutdown the system
+        // reboot - to reboot the system
+        // run -s <system> - to run a system
+        // run -s <system> -c <command> - to run a command on a system
+        // open -s <system> - to access a specific sub-program 
+        // exit -s <system> - to exit a specific sub-program
+        // help -s <system> - to get help on a specific sub-program
+        // shutdown -s <system> - to shutdown a specific sub-program
+    
+        // emergency - to activate emergency protocols
+        // emergency -s <system> - to activate emergency protocols on a specific system
 }
+
